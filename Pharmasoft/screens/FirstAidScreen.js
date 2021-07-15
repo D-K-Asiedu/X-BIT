e54ba4d60d620e8a0e45407fec06671ef4d03171
jkobby <fuachieluke@gmail.com>
2021-07-15 13:25:49 +0000

    Updated first aid screen

---
+++ Pharmasoft/screens/FirstAidScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class FirstAidScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text> First aid screen </Text>
+            </View>
+        )
+    }
+}
+
+export default FirstAidScreen
