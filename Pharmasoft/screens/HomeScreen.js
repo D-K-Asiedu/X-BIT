d26a2125629ba094a49d9fda606507d4b8affb8c
stwumasare <sethtwumasare15@gmail.com>
2021-07-17 09:41:06 +0000

    homescreen

---
+++ Pharmasoft/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { 
+    View, 
+    Text, 
+    TouchableOpacity, 
+    Dimensions,
+    StyleSheet,
+    StatusBar,
+    Image
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import LinearGradient from 'react-native-linear-gradient';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { useTheme } from '@react-navigation/native';
+
+const HomeScreen 
+
+export default FirstAidScreen
+
+const styles = StyleSheet.create
