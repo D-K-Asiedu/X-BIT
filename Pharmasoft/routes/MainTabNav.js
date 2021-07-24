a7717b380a94fd16e181a4f3b9ac3c00a5bb61a4
stwumasare <sethtwumasare15@gmail.com>
2021-07-24 08:36:08 +0000

    removed drawercontent and maintabscreen, added global styles and routes

---
+++ Pharmasoft/routes/MainTabNav.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from '../screens/HomeScreen'
+import ShopScreen from '../screens/ShopScreen'
+import FirstAidScreen from "../screens/FirstAidScreen";
+import { globalColours } from "../styles/global";
+import { Ionicons, Fontisto, FontAwesome5 } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+    return (
+        <Tab.Navigator
+            tabBarOptions={{
+                activeTintColor: globalColours.mainCol,
+                labelPosition: 'beside-icon',
+                keyboardHidesTabBar: true,
+            }}
+            screenOptions={({route}) => ({
+                tabBarIcon: ({color, size}) => {
+                    switch (route.name) {
+                        case 'Home':
+                            return <Ionicons name="home" size={25} color={color} />
+                        case 'Shop':
+                            return <Fontisto name="shopping-store" size={20} color={color} />
+                        case 'First-aid':
+                            return <FontAwesome5 name="first-aid" size={22} color={color} />
+    
+                    
+                        default:
+                            break;
+                    }
+                }
+            })}
+        >
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Shop" component={ShopScreen} />
+            <Tab.Screen name="First-aid" component={FirstAidScreen} />
+        </Tab.Navigator>
+    )
+}
+
+export default Tabs
---
+++ Pharmasoft/styles/global.js
@@ -0,0 +1,91 @@
+import {StyleSheet, Dimensions, StatusBar} from 'react-native'
+
+
+const height = Platform.OS === 'android' ? Dimensions.get('screen').height - StatusBar.currentHeight : Dimensions.get("window").height;
+
+const darkBlueBuffer = '#1a2e35'
+
+export const globalColours = {
+    mainCol: '#1ba665',
+    darkBlue: darkBlueBuffer,
+    greyBlue: '#375a64',
+    lightGrey: '#808080',
+    darkGrey: '#333333',
+    grey: '#696969',
+    firstAid:{
+        'plaster':'#f7e0b5',
+        'heart':'#b7dbfd',
+        'heart-flat':'#fcd0e5',
+        'lung': '#bbf8e2',
+        'bee': '#f9f48a',
+        'concussion':'#d5ffd8',
+        'fire': '#f5d9bf',
+        'cut': '#9cb1dd',
+        'eye': '#f9d2bd',
+        'poison': '#e9b9ff',
+        'bone': '#fcf5d4',
+        'choke':'#badbf9',    
+        'heat': '#f8cdcd',
+        'cold': '#a7f2f4',
+        'tooth':'#f9d3d3',
+        'injury': '#ecccba',
+        'nose': '#f5cccc',
+    }
+}
+
+export const globalStyles = StyleSheet.create({
+    container:{
+        flex: 1,
+        // height: height,
+        backgroundColor: globalColours.mainCol,
+    },
+    header:{
+        flexDirection: 'row',
+        height: 100,
+        backgroundColor: globalColours.mainCol,
+        paddingTop: 30,
+        paddingHorizontal: 20,
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    h2:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#ffffff',
+    },
+    h3:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: darkBlueBuffer,
+    },
+    content:{
+        flex: 1,
+        paddingTop: 2,
+        paddingHorizontal: 20,
+        backgroundColor: '#f2f2f2',
+        borderTopStartRadius: 30,
+        borderTopEndRadius: 30, 
+    },
+})
+
+export const images = {
+    firstAid:{
+        'plaster':require('../assets/first-aid-icons/plaster.png'),
+        'heart': require('../assets/first-aid-icons/heart.png'),
+        'heart-flat': require('../assets/first-aid-icons/heart-flat.png'),
+        'lung': require('../assets/first-aid-icons/lung.png'),
+        'bee': require('../assets/first-aid-icons/bee.png'),
+        'concussion': require('../assets/first-aid-icons/concussion.png'),
+        'fire': require('../assets/first-aid-icons/fire.png'),
+        'cut': require('../assets/first-aid-icons/cut.png'),
+        'eye': require('../assets/first-aid-icons/eye.png'),
+        'poison': require('../assets/first-aid-icons/poisons.png'),
+        'bone': require('../assets/first-aid-icons/bone.png'),
+        'choke': require('../assets/first-aid-icons/choke.png'),    
+        'heat': require('../assets/first-aid-icons/heat.png'),
+        'cold': require('../assets/first-aid-icons/cold.png'),
+        'tooth': require('../assets/first-aid-icons/tooth.png'),
+        'injury': require('../assets/first-aid-icons/injury.png'),
+        'nose': require('../assets/first-aid-icons/bleeding.png'),
+    }
+}
